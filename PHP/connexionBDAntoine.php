6cdbf4fd356eb31f70573d67b3ef45b48bde62f4
momoman852 <mohamed-masbahaboulaich@outlook.com>
2019-12-15 03:21:41 +0100

    changement d'architecture de modele conceptuel

--- PHP/connexionBDAntoine.php
+++
@@ -1,8 +0,0 @@
-<?php
-	try{
-		$dbh = new PDO('mysql:host=localhost;dbname=MyEvents;charset=UTF8','root','root',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,));
-	} catch(PDOException $e){
-		echo $e->getMessage();
-		die(" FUCK Connexion impossible !");
-	}
-?>
--- PHP/keyLog.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $user_name = "debian-sys-maint";
-    $psswd = "t5n8O9FMkWEb4ssP";
-
-?>
